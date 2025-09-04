5f2099c6ee72065f3fc169bc4686f93ae9542c7d
Plabss <plabonchowdhury9@gmail.com>
2025-09-04 23:22:58 +0600

    folder structure added

---
+++ template.sh
@@ -0,0 +1,19 @@
+# create directories
+mkdir -p src
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+
+touch app.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created."
+
+# run "sh template.sh" in bash terminal to create the above structure.
